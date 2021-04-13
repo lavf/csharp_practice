8996b8a91d32c010ae08f1163bec6a30456809f8
Leticia Valladares <73216174+lavf@users.noreply.github.com>
2021-04-13 19:55:15 +0200

    Console app for basic calculations using delegates

---
+++ AppFuerGrundberechnungen/AppFuerGrundberechnungen/Grundberechnungen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFuerGrundberechnungen
+{
+    // Class created for every basic calculation
+    class Grundberechnungen
+    {
+        // Every class type is "void" and every sentence written in the console is an explanation of the respective operation
+        public void Addition(double a, double b)
+        {
+            Console.WriteLine($"Summe: {a} plus {b} ist gleich {a + b}");
+        }
+
+        public void Substraction(double a, double b)
+        {
+            Console.WriteLine($"Substraktion: {a} minus {b} ist gleich {a - b}");
+        }
+
+        public void Multiplication(double a, double b)
+        {
+            Console.WriteLine($"Multiplikation: {a} mal {b} ist gleich {a * b}");
+        }
+
+        public void Division(double a, double b)
+        {
+            Console.WriteLine($"Division: {a} durch {b} ist gleich {a / b}");
+        }
+
+        
+    }
+}
---
+++ AppFuerGrundberechnungen/AppFuerGrundberechnungen/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AppFuerGrundberechnungen
+{
+    // Define "delegate", which is an indicator of method using the same method type 
+    // established in the Class "Grundberechnungen" file and data type for each parameter
+    public delegate void doubleDelegate(double a, double b);
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // Establish each parameter [local variables]
+            double a = 245.9;
+            double b = 0.00000003;
+            double c = 10.9;
+            double d = 3.00000003;
+            double e = 33.3;
+            double f = 56.1111113;
+
+            // Initialize the class as grundBe
+            Grundberechnungen grundBe = new Grundberechnungen();
+
+            // Instance the delegate as doubleDel
+            doubleDelegate doubleDel = grundBe.Addition;
+
+            //// Invoke the delegate and establish the values of the parameters
+            doubleDel(a, b);
+            
+            Console.WriteLine("=======================================================================");
+
+            //// Invoke the delegate and apply one or many methods of the class.
+            //// "Multicast Feature" (This is only valid for the same type of method [here 'void'])
+            //// ****Both methods "Multiplication" and "Addition" are called using the variables invoked at the end****
+            doubleDel += grundBe.Multiplication;
+
+            //// Invoke the delegate and establish the values of the parameters
+            doubleDel(c, d);
+
+            Console.WriteLine("=======================================================================");
+
+            //// Invoke the delegate and apply one or many methods of the class.
+            //// "Multicast Feature" (this is only valid for the same type of method [here 'void'])
+            //// ****The method "Multiplication" is removed and method "Substraction" is added****
+            doubleDel -= grundBe.Multiplication;
+            doubleDel += grundBe.Substraction;
+
+            //// Invoke the delegate and establish the values of the parameters
+            doubleDel(e, f);
+
+            // Conclusion: 
+            // - Methods indicated by delegates persist in the programm unless it is specified that they are removed from it.
+            // - Methods indicated by delegates take the variables specified AFTER them.
+            // - If no parameter is specified after a delegate, the delegate is not invoked (not deployed on the console).
+        }
+    }
+}
